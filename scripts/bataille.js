0399ba74646aad8ce25c29d4935a4c178e8f69da
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-09-21 18:30:18 +0200

    style test

--- scripts/bataille.js
+++ scripts/bataille.js
@@ -1,10 +1,12 @@
 const form = document.querySelector("form");
 const scoreLeft = document.querySelector(".left > span");
 const scoreRight = document.querySelector(".right > span");
+const leftCard = document.querySelector(".left-cards > span");
+const rightCard = document.querySelector(".right-cards > span");
 
 scoreLeft.textContent = deckLeft.length;
 scoreRight.textContent = deckRight.length;
+leftCard.textContent = deckLeft[0];
+rightCard.textContent = deckRight[0];
 
-form.addEventListener("submit", () => {
-  console.log(scoreLeft, scoreRight);
-});
+form.addEventListener("submit", () => {});
