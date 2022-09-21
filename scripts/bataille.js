6b50089bfa3318c3e471801ca7a0e496ef518d3d
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-09-21 19:55:39 +0200

    equal ?

--- scripts/bataille.js
+++ scripts/bataille.js
@@ -13,22 +13,49 @@ function display() {
 }
 
 function battle() {
-  if (deckLeft[n] > deckRight[n]) {
-    deckLeft.push(deckRight[0]);
-    deckRight.splice(0, 1);
-    let winCard = deckLeft[0];
-    deckLeft.splice(0, 1);
-    deckLeft.push(winCard);
-    display();
-  } else if (deckLeft[n] < deckRight[n]) {
-    deckRight.push(deckLeft[0]);
-    deckLeft.splice(0, 1);
-    let winCard = deckRight[0];
-    deckRight.splice(0, 1);
-    deckRight.push(winCard);
-    display();
+  if (n > 0) {
+    if (deckLeft[n] > deckRight[n]) {
+      for (let i = n + 1; i > 0; i--) {
+        deckLeft.push(deckRight[n]);
+        deckRight.splice(n, 1);
+        let winCard = deckLeft[n];
+        deckLeft.splice(n, 1);
+        deckLeft.push(winCard);
+        n--;
+      }
+    } else if (deckLeft[n] < deckRight[n]) {
+      for (let i = n + 1; i > 0; i--) {
+        deckRight.push(deckLeft[n]);
+        deckRight.splice(n, 1);
+        let winCard = deckRight[n];
+        deckRight.splice(n, 1);
+        deckRight.push(winCard);
+        n--;
+      }
+    } else {
+      n++;
+      console.log(n);
+    }
+    console.log(n);
   } else {
-    console.log("Egalité");
+    n = 0;
+    if (deckLeft[0] > deckRight[0]) {
+      deckLeft.push(deckRight[0]);
+      deckRight.splice(0, 1);
+      let winCard = deckLeft[0];
+      deckLeft.splice(0, 1);
+      deckLeft.push(winCard);
+      display();
+    } else if (deckLeft[0] < deckRight[0]) {
+      deckRight.push(deckLeft[0]);
+      deckLeft.splice(0, 1);
+      let winCard = deckRight[0];
+      deckRight.splice(0, 1);
+      deckRight.push(winCard);
+      display();
+    } else {
+      n++;
+    }
   }
 }
 
