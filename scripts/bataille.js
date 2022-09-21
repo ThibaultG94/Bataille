db5d2cc8cf8345c3f46df09b4f398cdf5de5bd51
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-09-21 19:09:27 +0200

    Bataille sauf égalité...

--- scripts/bataille.js
+++ scripts/bataille.js
@@ -3,10 +3,37 @@ const scoreLeft = document.querySelector(".left > span");
 const scoreRight = document.querySelector(".right > span");
 const leftCard = document.querySelector(".left-cards > span");
 const rightCard = document.querySelector(".right-cards > span");
+let n = 0;
 
-scoreLeft.textContent = deckLeft.length;
-scoreRight.textContent = deckRight.length;
-leftCard.textContent = deckLeft[0];
-rightCard.textContent = deckRight[0];
+function display() {
+  scoreLeft.textContent = deckLeft.length;
+  scoreRight.textContent = deckRight.length;
+  leftCard.textContent = deckLeft[0];
+  rightCard.textContent = deckRight[0];
+}
 
-form.addEventListener("submit", () => {});
+function battle() {
+  if (deckLeft[n] > deckRight[n]) {
+    deckLeft.push(deckRight[0]);
+    deckRight.splice(0, 1);
+    let winCard = deckLeft[0];
+    deckLeft.splice(0, 1);
+    deckLeft.push(winCard);
+    display();
+  } else if (deckLeft[n] < deckRight[n]) {
+    deckRight.push(deckLeft[0]);
+    deckLeft.splice(0, 1);
+    let winCard = deckRight[0];
+    deckRight.splice(0, 1);
+    deckRight.push(winCard);
+    display();
+  } else {
+    console.log("Egalité");
+  }
+}
+
+display();
+
+form.addEventListener("submit", () => {
+  battle();
+});
