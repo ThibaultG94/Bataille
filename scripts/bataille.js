84640b719514b67d708bf5dfe7580a469db8ae1f
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-09-21 21:46:55 +0200

    function equal git add .!

--- scripts/bataille.js
+++ scripts/bataille.js
@@ -4,6 +4,12 @@ const scoreRight = document.querySelector(".right > span");
 const leftCard = document.querySelector(".left-cards > span");
 const rightCard = document.querySelector(".right-cards > span");
 let n = 0;
+let left = false;
+let right = false;
+
+//-------------------------------------------------
+// FONCTION AFFICHAGE DU SCORE ET DES CARTES
+//-------------------------------------------------
 
 function display() {
   scoreLeft.textContent = deckLeft.length;
@@ -12,10 +18,32 @@ function display() {
   rightCard.textContent = deckRight[0];
 }
 
-function battle() {
+//----------------------------------------------------
+// FONCTION ENLEVEMENT DES CARTES EN CAS D'EGALITE
+//----------------------------------------------------
+
+function removeCards() {
+  let removeCardsLeft = document.querySelector(".left-cards > .delete");
+  let removeCardsRight = document.querySelector(".right-cards > .delete");
+  if (removeCardsLeft) {
+    removeCardsLeft.remove();
+    removeCardsRight.remove();
+    removeCards();
+  } else if (removeCardsRight) {
+    removeCardsLeft.remove();
+    removeCardsRight.remove();
+    removeCards();
+  }
+}
+
+//-----------------------------------------
+// FONCTION EGALITE DES CARTES
+//-----------------------------------------
+
+function equalBattle() {
   if (n > 0) {
     if (deckLeft[n] > deckRight[n]) {
-      for (let i = n + 1; i > 0; i--) {
+      for (let i = n; i > 0; i--) {
         deckLeft.push(deckRight[n]);
         deckRight.splice(n, 1);
         let winCard = deckLeft[n];
@@ -23,20 +51,64 @@ function battle() {
         deckLeft.push(winCard);
         n--;
       }
+      left = true;
+      removeCards();
+      equalBattle();
     } else if (deckLeft[n] < deckRight[n]) {
-      for (let i = n + 1; i > 0; i--) {
+      for (let i = n; i > 0; i--) {
         deckRight.push(deckLeft[n]);
-        deckRight.splice(n, 1);
+        deckLeft.splice(n, 1);
         let winCard = deckRight[n];
         deckRight.splice(n, 1);
         deckRight.push(winCard);
         n--;
       }
+      right = true;
+      removeCards();
+      equalBattle();
     } else {
       n++;
-      console.log(n);
+      let newSpanLeft = document.createElement("span");
+      let newSpanRight = document.createElement("span");
+      document.querySelector(".left-cards").append(newSpanLeft);
+      document.querySelector(".right-cards").append(newSpanRight);
+      newSpanLeft.classList = "delete";
+      newSpanRight.classList = "delete";
+      newSpanLeft.textContent = deckLeft[n];
+      newSpanRight.textContent = deckRight[n];
     }
-    console.log(n);
+  } else {
+    if (left) {
+      deckLeft.push(deckRight[0]);
+      deckRight.splice(0, 1);
+      let winCard = deckLeft[0];
+      deckLeft.splice(0, 1);
+      deckLeft.push(winCard);
+      display();
+      left = false;
+      right = false;
+    } else if (right) {
+      deckRight.push(deckLeft[0]);
+      deckLeft.splice(0, 1);
+      let winCard = deckRight[0];
+      deckRight.splice(0, 1);
+      deckRight.push(winCard);
+      display();
+      left = false;
+      right = false;
+    } else {
+      alert("Error");
+    }
+  }
+}
+
+//------------------------------------
+// FONCTION PRINCIPALE
+//------------------------------------
+
+function battle() {
+  if (n > 0) {
+    equalBattle();
   } else {
     n = 0;
     if (deckLeft[0] > deckRight[0]) {
@@ -55,12 +127,25 @@ function battle() {
       display();
     } else {
       n++;
+      let newSpanLeft = document.createElement("span");
+      let newSpanRight = document.createElement("span");
+      document.querySelector(".left-cards").append(newSpanLeft);
+      document.querySelector(".right-cards").append(newSpanRight);
+      newSpanLeft.classList = "delete";
+      newSpanRight.classList = "delete";
+      newSpanLeft.textContent = deckLeft[n];
+      newSpanRight.textContent = deckRight[n];
     }
   }
 }
-
 display();
+
+//-------------------------
+// BOUTON JOUER
+//-------------------------
 
 form.addEventListener("submit", () => {
   battle();
+  console.log(deckLeft);
+  console.log(deckRight);
 });
